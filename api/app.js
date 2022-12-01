9836f0fe268183dce5205a1f764e31ddbc52bf26
RenSoon <sbrendonsamson@gmail.com>
2022-12-01 20:05:27 +0800

    CRUD

--- api/app.js
+++ api/app.js
@@ -9,8 +9,9 @@ app.use(cors());
 
 let users = require('./users.js');
 
-app.use(users.routes()).use(users.allowedMethods());
+app.use(users.routes()).use(users.allowedMethods);
+
 app.listen(3001, function() {
     console.log("Server at http://localhost:3001/users")
-})
+});
 
